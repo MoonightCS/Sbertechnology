54450c28f4ef445f5d7bf668919cbad6e206117a
Moonlight <bogdan.popov@mail.ru>
2016-07-24 16:57:45 +0300

    package has been changed, test has been added, .gitignore added

--- hw/src/ru/sbt/bodya/hw2/Person.java
+++ hw/src/ru/sbt/bodya/hw2/Person.java
@@ -1,4 +1,4 @@
-package ru.sbt.bodya.hw;
+package ru.sbt.bodya.hw2;
 
 public class Person {
 
@@ -16,6 +16,14 @@ public class Person {
         this.name = name;
 
         this.spouse = null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Person getSpouse() {
+        return spouse;
     }
 
     /**
@@ -69,46 +77,6 @@ public class Person {
             return false;
         }
 
-    }
-
-
-    public static void main(String[] args) {
-
-        Person man1 = new Person(true, "Bogdan");
-        Person woman1 = new Person(false, "Katya");
-        Person man2 = new Person(true, "Petya");
-        Person woman2 = new Person(false, "Lena");
-
-        man1.marry(woman1);
-        man2.marry(woman2);
-        man1.marry(woman2);
-        man2.marry(woman1);
-        man2.divorce();
-
-        if (man1.spouse != null) {
-            System.out.printf("man1 wife = %s\n", man1.spouse.name);
-        }
-        if (woman1.spouse != null) {
-            System.out.printf("woman1 husband = %s\n", woman1.spouse.name);
-        }
-        if (man2.spouse != null) {
-            System.out.printf("man2 wife = %s\n", man2.spouse.name);
-        }
-        if (woman2.spouse != null) {
-            System.out.printf("woman2 husband = %s\n", woman2.spouse.name);
-        }
-
-        if (man1.marry(woman2) == false) {
-            System.out.println("False в методе marry рабоатет");
-        }
-
-        if (man2.marry(man1) == false) {
-            System.out.println("False в методе marry работает");
-        }
-
-        if (man2.divorce() == false) {
-            System.out.println("False в методе divorce работает");
-        }
     }
 
 }
